8c1551f54acb8b385b9a07f91f7d488572db5b35
arpanbhattacharya <apubhattacharya71@gmail.com>
2022-09-27 12:32:34 +0530

    added 1st product

--- admin/Controller/product/insert_product.php
+++ admin/Controller/product/insert_product.php
@@ -10,8 +10,6 @@ $price=$_POST["price"];
 $s_price=$_POST["s_price"];
 $warranty=$_POST["warranty"];
 
-print_r($file);
-
 $fileName=$file["name"];
 $fileExt=end(explode(".", $fileName));
 $filePath=$file["tmp_name"];
@@ -19,12 +17,11 @@ $filePath=$file["tmp_name"];
 $allowtype=array("jpg","jpeg","png","gif");
 
 if(in_array($fileExt,$allowtype)){
-    echo "Okay";
     $f_name=time().".".$fileExt;
     $destination="../../product_image/".$f_name;
     move_uploaded_file($filePath,$destination);
 
-    $sql="INSERT INTO products(file,category,p_name,p_code,description,price,s_price,warranty)
+    $sql="INSERT INTO product(image,category,p_name,p_code,description,price,s_price,warranty)
         VALUES('$f_name','$categories','$p_name','$p_code','$description','$price','$s_price','$warranty')";
 
     $con->query($sql);    
